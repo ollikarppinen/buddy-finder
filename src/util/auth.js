ddb614b6766904989ddfb29d62428903103cda1e
Karppinen Olli <olli.karppinen@aalto.fi>
2019-10-06 22:42:24 +0300

    Adds firebase .env config

--- src/util/auth.js
+++ src/util/auth.js
@@ -5,10 +5,14 @@ import "firebase/auth";
 
 // Replace with your own Firebase credentials
 firebase.initializeApp({
-  apiKey: "AIzaSyBkkFF0XhNZeWuDmOfEhsgdfX1VBG7WTas",
-  authDomain: "divjoy-demo.firebaseapp.com",
-  projectId: "divjoy-demo",
-  appID: "divjoy-demo"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 });
 
 const authContext = createContext();
