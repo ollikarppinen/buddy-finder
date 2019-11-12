b734dd39bb5fe9ff67db71c8c5eef2de00951c0f
Karppinen Olli <olli.karppinen@aalto.fi>
2019-11-12 11:13:47 +0200

    Updates dashboard message

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -21,7 +21,7 @@ function DashboardPage(props) {
       color="white"
       size="large"
       title="Dashboard"
-      subtitle="Dashboard components are coming to the Divjoy library soon. For now, you can implement a custom dashboard here after exporting your code."
+      subtitle="Coming soon...™"
     />
   )
 }
