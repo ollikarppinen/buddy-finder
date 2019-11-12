8102cff662204790fb4667cc4a58d99a7a43c38f
Karppinen Olli <olli.karppinen@aalto.fi>
2019-11-12 18:06:47 +0200

    Adds nav icons

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -43,6 +43,9 @@ function Navbar(props) {
           {auth.user && (
             <div className="navbar-end">
               <Link className="navbar-item" to="/events" onClick={closeMenu}>
+                <span class="icon is-medium">
+                  <i class="far fa-calendar"></i>
+                </span>
                 Events
               </Link>
               <div
@@ -54,6 +57,9 @@ function Navbar(props) {
                   className="navbar-link"
                   onClick={() => setAccountMenuOpen(!accountMenuOpen)}
                 >
+                  <span class="icon is-medium">
+                    <i class="far fa-user"></i>
+                  </span>
                   Account
                 </div>
                 <div className="navbar-dropdown is-boxed">
@@ -62,6 +68,9 @@ function Navbar(props) {
                     to="/account"
                     onClick={closeMenu}
                   >
+                    <span class="icon is-medium">
+                      <i class="fas fa-cog"></i>
+                    </span>
                     Profile
                   </Link>
                   <Link
@@ -69,6 +78,9 @@ function Navbar(props) {
                     to="/dashboard"
                     onClick={closeMenu}
                   >
+                    <span class="icon is-medium">
+                      <i class="fas fa-bell"></i>
+                    </span>
                     Dashboard
                   </Link>
                   <Link
@@ -76,6 +88,9 @@ function Navbar(props) {
                     to="/create_event"
                     onClick={closeMenu}
                   >
+                    <span class="icon is-medium">
+                      <i class="fas fa-calendar-plus"></i>
+                    </span>
                     Create event
                   </Link>
                   <Link
@@ -87,6 +102,9 @@ function Navbar(props) {
                       auth.signout()
                     }}
                   >
+                    <span class="icon is-medium">
+                      <i class="fas fa-sign-out-alt"></i>
+                    </span>
                     Sign out
                   </Link>
                 </div>
